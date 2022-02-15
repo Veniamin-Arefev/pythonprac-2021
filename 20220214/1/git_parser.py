db962678be38acee17120195800c4ac884e7cc36
Veniamin_Arefev <veniamin.arefev@mail.ru>
2022-02-15 07:35:39 +0300

    Done 20220214/1.2

--- 20220214/1/git_parser.py
+++ 20220214/1/git_parser.py
@@ -11,36 +11,41 @@ except BaseException as e:
     print('Please execute "pip install colorama"')
     exit(0)
 
+prefix = '.git'
 branches = os.listdir(".git/refs/heads")
 
 if len(sys.argv) <= 1:
     print(f"{Fore.RED}Target branch not specified.{Fore.RESET} Possible variants is {', '.join(branches)}. "
-          f"Switching to {Fore.CYAN}{(repo := branches[0])}{Fore.RESET}.")
+          f"Switching to {Fore.CYAN}{(branch_name := branches[0])}{Fore.RESET}.")
 else:
     if sys.argv[1] not in branches:
         print(f"{Fore.RED}Target branch not found.{Fore.RESET} Possible variants is {', '.join(branches)}. "
-              f"Switching to {Fore.CYAN}{(repo := branches[0])}{Fore.RESET}.")
+              f"Switching to {Fore.CYAN}{(branch_name := branches[0])}{Fore.RESET}.")
     else:
-        print(f"Parsing {(repo := sys.argv[1])} branch.")
+        print(f"Parsing {(branch_name := sys.argv[1])} branch.")
+
+with open(join(prefix, 'refs', 'heads', branch_name), 'r') as file:
+    commit_id = file.read().strip()
 
 # repo_path = join(repo, 'objects', '??', '*')
 
 # for obj_name in iglob(repo_path):
-#     print(Fore.GREEN, obj_name, Fore.RESET)
-#
-#     with open(obj_name, 'rb') as file:
-#         full_obj = zlib.decompress(file.read())
-#         header, _, body = full_obj.partition(b'\x00')
-#         kind, size = header.split()
-#         print(Fore.BLUE, kind.decode(), int(size), Fore.RESET)
-#         if kind == b'commit':
-#             print(body.decode())
-#         elif kind == b'tree':
-#             while body:
-#                 treehdr, _, tail = body.partition(b'\x00')
-#                 gitid, body = tail[:20], tail[20:]
-#                 print(f'\t{treehdr}, {gitid.hex()}')
-#         elif kind == b'blob':
-#             print("There must be info about blob")
-#         else:
-#             print(Fore.RED, "ERROR TYPE", Fore.RESET)
+obj_name = join(prefix, 'objects', commit_id[:2], commit_id[2:])
+print(Fore.GREEN, obj_name, Fore.RESET, sep='')
+
+with open(obj_name, 'rb') as file:
+    full_obj = zlib.decompress(file.read())
+    header, _, body = full_obj.partition(b'\x00')
+    kind, size = header.split()
+    print(f"{Fore.BLUE}Type : {kind.decode():8} with {int(size):5} bytes{Fore.RESET}")
+    if kind == b'commit':
+        print(body.decode())
+    elif kind == b'tree':
+        while body:
+            treehdr, _, tail = body.partition(b'\x00')
+            gitid, body = tail[:20], tail[20:]
+            print(f'\t{treehdr}, {gitid.hex()}')
+    elif kind == b'blob':
+        print("There must be info about blob")
+    else:
+        print(Fore.RED, "ERROR TYPE", Fore.RESET)
