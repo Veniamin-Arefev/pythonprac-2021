cce5f61a71bc8b82cd97e5bc11fe1062416daa87
Veniamin-Arefev <veniamin.arefev@mail.ru>
2022-03-03 15:18:33 +0300

    Task 1. Done 2.2

--- 20220228/1/task.py
+++ 20220228/1/task.py
@@ -1,7 +1,7 @@
 import textdistance
 
 
-def dist(s1, s2):
+def dist(s1, s2, s3):
     return textdistance.levenshtein.distance(s1, s2)
 
 
@@ -9,4 +9,4 @@ str1, str2 = input().replace(' ', ''), input().replace(' ', '')
 
 str3 = input().replace(' ', '')
 
-result = dist(str1, str2)
+result = dist(str1, str2, str3)
